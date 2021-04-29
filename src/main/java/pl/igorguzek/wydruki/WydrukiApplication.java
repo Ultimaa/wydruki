180b464b948d10d30f200a9ee6d07e5f99fccb97
Igor Guzek <ultimaa@gmail.com>
2021-04-29 21:40:49 +0200

    Initial commit

---
+++ src/main/java/pl/igorguzek/wydruki/WydrukiApplication.java
@@ -0,0 +1,13 @@
+package pl.igorguzek.wydruki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WydrukiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WydrukiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/igorguzek/wydruki/WydrukiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.igorguzek.wydruki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WydrukiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
