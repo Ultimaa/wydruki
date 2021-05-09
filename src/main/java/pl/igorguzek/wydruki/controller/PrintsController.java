0c19afceb9485f28f20107d197a7877d98faa27e
Igor Guzek <ultimaa@gmail.com>
2021-05-09 22:52:20 +0200

    Add: -SSL Suport -Prints-set page -And liitle fixes in code

--- src/main/java/pl/igorguzek/wydruki/controller/PrintsController.java
+++ src/main/java/pl/igorguzek/wydruki/controller/PrintsController.java
@@ -3,12 +3,13 @@ package pl.igorguzek.wydruki.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HomeController {
 
-    @GetMapping("/")
-    public String index(){
-        return "index";
+@Controller
+public class PrintsController {
+
+    @GetMapping("/prints-set")
+    public String index() {
+        return "prints/prints-set";
     }
 
 }
